2119f0cdd14cb8e32dd9d2b6f253f4829dc54cb6
Stefan Ziegler <2767800+edigonzales@users.noreply.github.com>
2026-08-10 18:27:39 +0000

    verify installed metamodel header

--- test/consumer/installed-package/cpp-main.cpp
+++ test/consumer/installed-package/cpp-main.cpp
@@ -1,8 +1,11 @@
 #include "ilic/Compiler.h"
+#include "metamodel/MetaModel.h"
 
 #include <string>
 
 int main()
 {
+    static_assert(sizeof(metamodel::Model) > 0,
+                  "installed SDK must expose the concrete metamodel API");
     return std::string(ilic::version()).empty() ? 1 : 0;
 }
