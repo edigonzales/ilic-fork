aa6d0abf532b6c29e4278420d1aa8f4c6b4029cd
Stefan Ziegler <2767800+edigonzales@users.noreply.github.com>
2026-08-11 06:02:08 +0000

    test installed metamodel store header

--- test/consumer/installed-package/cpp-main.cpp
+++ test/consumer/installed-package/cpp-main.cpp
@@ -2,6 +2,7 @@
 
 #ifdef ILIC_TEST_METAMODEL_HEADER
 #include "metamodel/MetaModel.h"
+#include "metamodel/MetaModelStore.h"
 #endif
 
 #include <string>
@@ -11,6 +12,8 @@ int main()
 #ifdef ILIC_TEST_METAMODEL_HEADER
     static_assert(sizeof(metamodel::Model) > 0,
                   "installed SDK must expose the concrete metamodel API");
+    static_assert(sizeof(metamodel::MetaModelStore) > 0,
+                  "installed SDK must expose the concrete metamodel store API");
 #endif
     return std::string(ilic::version()).empty() ? 1 : 0;
 }
